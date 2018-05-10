07fb4c53a1f6dda0d644d632d97a3109443e48fe
Jason Zhao <jasonzhao6@gmail.com>
2018-05-10 11:54:52 -0700

    Run AppStart instead of App in lectures

--- 04/04-lecture/src/index.js
+++ 04/04-lecture/src/index.js
@@ -7,6 +7,6 @@ Edit App.js
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./AppStart";
 
 ReactDOM.render(<App />, document.getElementById("root"));
