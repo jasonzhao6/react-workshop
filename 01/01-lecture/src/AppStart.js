05d3ca8d7fade75d48b4a8110e4a50ad180204de
Jason Zhao <jasonzhao6@gmail.com>
2018-05-10 10:13:40 -0700

    Turn imperative callbacks into reactive ones

--- 01/01-lecture/src/AppStart.js
+++ 01/01-lecture/src/AppStart.js
@@ -3,14 +3,39 @@ import createOscillator from "./createOscillator";
 import "./index.css";
 
 class App extends React.Component {
+  state = {
+    isPlaying: false,
+    volume: 0.5,
+    pitch: 0.5
+  }
+
   oscillator = createOscillator();
 
+  componentDidMount() {
+    this.doImperativeStuff();
+  }
+
+  componentDidUpdate() {
+    this.doImperativeStuff();
+  }
+
+  doImperativeStuff() {
+    let { isPlaying, pitch, volume } = this.state;
+    if (isPlaying) {
+      this.oscillator.play();
+    } else {
+      this.oscillator.stop();
+    }
+    this.oscillator.setPitchBend(pitch);
+    this.oscillator.setVolume(volume);
+  }
+
   play = () => {
-    this.oscillator.play();
+    this.setState({ isPlaying: true });
   };
 
   stop = () => {
-    this.oscillator.stop();
+    this.setState({ isPlaying: false });
   };
 
   changeTone = event => {
@@ -18,8 +43,7 @@ class App extends React.Component {
     const { top, right, bottom, left } = event.target.getBoundingClientRect();
     const pitch = (clientX - left) / (right - left);
     const volume = 1 - (clientY - top) / (bottom - top);
-    this.oscillator.setPitchBend(pitch);
-    this.oscillator.setVolume(volume);
+    this.setState({ pitch, volume });
   };
 
   render() {
