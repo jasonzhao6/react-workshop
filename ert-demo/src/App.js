a2c20691f11910612fad1392c89368c968fa3908
Jason Zhao <jasonzhao6@gmail.com>
2018-05-28 21:47:09 -0700

    Initial demo state

--- ert-demo/src/App.js
+++ ert-demo/src/App.js
@@ -1,18 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import classNames from 'classnames';
+import * as icons from 'react-icons/lib/fa';
+import * as texts from './texts';
+import './index.css';
 
-class App extends Component {
+const DATA = [
+  { label: <icons.FaAutomobile />,   content: <texts.Cars /> },
+  { label: <icons.FaBed />,          content: <texts.Hotels /> },
+  { label: <icons.FaPlane />,        content: <texts.Flights /> },
+  { label: <icons.FaSpaceShuttle />, content: <texts.Space /> }
+];
+
+class Tabs extends PureComponent {
+  // Override constructor to init state and bind callbacks.
+  constructor(props) {
+    super(props);
+
+    this.state = { index: 0 };
+
+    this.selectTab = this.selectTab.bind(this);
+  }
+
+  // Callback method to select a tab on click.
+  selectTab(index) {
+    this.setState({ index });
+  }
+
+  // Render all tab labels and currently selected content.
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='tabs'>
+        <div className='tabsLabels'>
+          { this.props.data.map((tab, index) => {
+            const isSelected = index === this.state.index;
+            return (
+              <div
+                className={ classNames('tabsLabel', { isSelected }) }
+                key={ index }
+                onClick={ this.selectTab.bind(null, index) }
+              >
+                { tab.label }
+              </div>
+            );
+          }) }
+        </div>
+
+        <div className='tabsContent'>
+          { this.props.data[this.state.index].content }
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends PureComponent {
+  // Render DATA using <Tabs />.
+  render() {
+    return (
+      <div className='App'>
+        <Tabs data={ DATA } />
       </div>
     );
   }
---
+++ ert-demo/src/texts.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export const Cars = () => (
+  <div>
+    <h2>Car rentals</h2>
+    <p>We offer the finest selection of cars:</p>
+    <ul>
+      <li>SUVs</li>
+      <li>Sedans</li>
+      <li>Coupes</li>
+      <li>Roadsters</li>
+      <li>Monster Trucks</li>
+      <li>Three Wheelers</li>
+    </ul>
+  </div>
+);
+
+export const Hotels = () => (
+  <div>
+    <h2>Hotels</h2>
+    <p>
+      The best hotels at the best price. Whether you’re looking for a romantic
+      get-away or just need a place to crash that offers free breakfast and a
+      hot tub, we have the best hotels around.
+    </p>
+  </div>
+);
+
+export const Flights = () => (
+  <div>
+    <h2>Flights</h2>
+    <p>
+      Cheap fare and great flights. We have the most non-stop from major cities
+      across the world than anybody else.
+    </p>
+  </div>
+);
+
+export const Space = () => (
+  <div>
+    <h2>Space</h2>
+    <p>
+      Sometimes when I watch shows like Passengers, and they’re experiencing
+      intergallactic travel, slingshotting around a star and gazing at it out
+      the window, or standing outside the ship staring into the universe, I get
+      actually angry that I won’t experience it. Time to find a religion that
+      believes we can create worlds and travel from one to the other.
+    </p>
+  </div>
+);
